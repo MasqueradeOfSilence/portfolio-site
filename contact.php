48d96df87d35132addc2c1ddc454092589b7d471
MasqueradeOfSilence <alexcn711@gmail.com>
2020-10-15 19:15:37 -0600

    initial work -- some issues exist on larger websites

---
+++ contact.php
@@ -0,0 +1,32 @@
+<?php
+  if($_POST["message"])
+  {
+    if ($_POST["mathQuestion"] == 45)
+    {
+      mail("softwaredev@alexanderneville.com", $_POST["subject"],
+        $_POST["message"], "From: " . $_POST["name"] . ", " . $_POST["email"]);
+
+      echo '<script type="text/JavaScript">
+        window.alert("Email sent successfully.");
+        </script>';
+
+      echo '<script type="text/JavaScript">
+        window.location = document.referrer;
+        </script>';
+
+      exit;
+    }
+    else
+    {
+      echo '<script type="text/JavaScript">
+        window.alert("Email failed.");
+        </script>';
+
+      echo '<script type="text/JavaScript">
+        window.location = document.referrer;
+        </script>';
+
+      exit;
+    }
+  }
+?>
